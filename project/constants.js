43b3690ba8b0a15619a383637ba3c5cd568022d4
David Vaz <davidzav13@gmail.com>
2019-12-31 00:01:12 +0000

    dinamic width and height

--- project/constants.js
+++ project/constants.js
@@ -1,10 +1,13 @@
+let width = Math.min(window.screen.availWidth, window.innerWidth);
+let height = Math.min(window.screen.availHeight, window.innerHeight)
+
 export default {
-    WIDTH : 1665,
-    HEIGHT : 860,
+    WIDTH : width,
+    HEIGHT : height,
     SPACE : 32,
-    PLAYER_STARTING_X : 1665 / 2,
-    PLAYER_STARTING_Y : 860,
+    PLAYER_STARTING_X : width / 2,
+    PLAYER_STARTING_Y : height,
     PLAYER_STARTING_RADIUS : 20,
-    SYSTEM_STARTING_X : 1665 / 2,
-    SYSTEM_STARTING_Y : 860 / 6
+    SYSTEM_STARTING_X : width / 2,
+    SYSTEM_STARTING_Y : height / 6
 };
