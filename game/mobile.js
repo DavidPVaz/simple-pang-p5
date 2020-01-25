ae3b45ea198bedc3fcc77cb5aade1856214945c1
David Vaz <davidzav13@gmail.com>
2020-01-25 18:53:05 +0000

    update last commit with handlers. still need to style the control buttons

--- game/mobile.js
+++ game/mobile.js
@@ -1,5 +1,22 @@
 const mobileAgents = ['android', 'iphone', 'ipad', 'blackberry', 'windows phone', 'webos'];
-const controls = ['up', 'left', 'right', 'shoot'];
+const keys = { 
+    up : 38,
+    left: 37, 
+    right: 39, 
+    shoot: 32
+};
+
+const touchStartHandlers = Object.keys(keys).map(function(control) {
+    return function() {
+        document.dispatchEvent(new KeyboardEvent('keydown', { keyCode: keys[control] }));
+    };
+});
+
+const touchEndHandlers = Object.keys(keys).map(function(control) {
+    return function() {
+        document.dispatchEvent(new KeyboardEvent('keyup', { keyCode: keys[control] }));
+    };
+});
 
 function isMobile() {
     return mobileAgents.some(function(userAgent) {
@@ -9,22 +26,19 @@ function isMobile() {
 
 function addMobileControls() {
 
-    arrayWithTouchStartHandlers
-    arrayWithTouchEndHandlers
-    
-    controls.forEach(function(control){
+    Object.keys(keys).forEach(function(control, index){
 
         let button = document.createElement('BUTTON');
         button.id = control;
-        button.ontouchstart = arrayWithTouchStartHandlers[index];
-        button.ontouchend = arrayWithTouchEndHandlers[index];
+        button.ontouchstart = touchStartHandlers[index];
+        button.ontouchend = touchEndHandlers[index];
         document.body.appendChild(button);
     });
 }
 
 function removeMobileControls() {
 
-    controls.forEach(function(control) {
+    Object.keys(keys).forEach(function(control) {
 
         let button = document.querySelector(`#${control}`); 
         
